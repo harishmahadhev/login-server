0ef7aeab201ca043340d338c042fc97bb437b3a4
harishmahadhev <harishmahadhev@gmail.com>
2021-07-28 13:14:25 +0530

    ..

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import "./database/db_connection.js";
 import { loginRouter } from "./routes/login_route.js";
 import cors from "cors";
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(json());
 app.use(cors());
 app.use('/login', loginRouter);
